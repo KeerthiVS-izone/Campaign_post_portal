d75c948b2288650b788267af643796f25161fb9c
dharani-izonetech <dharani.izonetech@gmail.com>
2026-03-12 20:20:54 +0530

    added api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://tweetapi.risingsuntech.in',
   headers: { 'Content-Type': 'application/json' },
 });
 
